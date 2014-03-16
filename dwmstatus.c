0019cb1a303dd9205827f7d07c760693d9f08791
Serguey Parkhomovsky <xindigo@gmail.com>
2014-03-16 11:42:34 -0700

    indentation changes and fixing memory leaks in getbattery function

--- dwmstatus.c
+++ dwmstatus.c
@@ -21,131 +21,147 @@ static Display *dpy;
 char *
 smprintf(char *fmt, ...)
 {
-	va_list fmtargs;
-	char *ret;
-	int len;
+    va_list fmtargs;
+    char *ret;
+    int len;
 
-	va_start(fmtargs, fmt);
-	len = vsnprintf(NULL, 0, fmt, fmtargs);
-	va_end(fmtargs);
+    va_start(fmtargs, fmt);
+    len = vsnprintf(NULL, 0, fmt, fmtargs);
+    va_end(fmtargs);
 
-	ret = malloc(++len);
-	if (ret == NULL) {
-		perror("malloc");
-		exit(1);
-	}
+    ret = malloc(++len);
+    if (ret == NULL) {
+        perror("malloc");
+        exit(1);
+    }
 
-	va_start(fmtargs, fmt);
-	vsnprintf(ret, len, fmt, fmtargs);
-	va_end(fmtargs);
+    va_start(fmtargs, fmt);
+    vsnprintf(ret, len, fmt, fmtargs);
+    va_end(fmtargs);
 
-	return ret;
+    return ret;
 }
 
 void
 settz(char *tzname)
 {
-	setenv("TZ", tzname, 1);
+    setenv("TZ", tzname, 1);
 }
 
 char *
 mktimes(char *fmt, char *tzname)
 {
-	char buf[129];
-	time_t tim;
-	struct tm *timtm;
+    char buf[129];
+    time_t tim;
+    struct tm *timtm;
 
-	memset(buf, 0, sizeof(buf));
-    
+    memset(buf, 0, sizeof(buf));
+
     if (tzname != NULL) {
         settz(tzname);
     }
-	tim = time(NULL);
-	timtm = localtime(&tim);
-	if (timtm == NULL) {
-		perror("localtime");
-		exit(1);
-	}
+    tim = time(NULL);
+    timtm = localtime(&tim);
+    if (timtm == NULL) {
+        perror("localtime");
+        exit(1);
+    }
 
-	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
-		fprintf(stderr, "strftime == 0\n");
-		exit(1);
-	}
+    if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
+        fprintf(stderr, "strftime == 0\n");
+        exit(1);
+    }
 
-	return smprintf("%s", buf);
+    return smprintf("%s", buf);
+}
+
+long
+read_long(const char *path)
+{
+    long i = 0;
+    FILE *fd;
+
+    if (!(fd = fopen(path, "r")))
+        return -1;
+
+    fscanf(fd, "%ld", &i);
+    fclose(fd);
+    return i;
+}
+
+char
+get_status()
+{
+    FILE *fp;
+    char st;
+
+    if ((fp = fopen(BATT_STATUS, "r")) == NULL)
+        return '?';
+
+    st = fgetc(fp);
+    fclose(fp);
+
+    switch(st) {
+        case 'C': return '+';
+        case 'D': return '-';
+        case 'F': return '=';
+        default : return '?';
+    }
 }
 
 char *
 getbattery()
 {
-    long lnum1, lnum2,percent = 0;
-    char *status = malloc(sizeof(char)*8);
+    long lnum1, lnum2, percent;
     const char *color;
-    char s = '?';
-    FILE *fp = NULL;
-    if ((fp = fopen(BATT_NOW, "r"))) {
-        fscanf(fp, "%ld\n", &lnum1);
-        fclose(fp);
-        fp = fopen(BATT_FULL, "r");
-        fscanf(fp, "%ld\n", &lnum2);
-        fclose(fp);
-        fp = fopen(BATT_STATUS, "r");
-        fscanf(fp, "%s\n", status);
-        fclose(fp);
+    char s;
 
-        percent = (lnum1/(lnum2/100));
+    lnum1 = read_long(BATT_NOW);
+    lnum2 = read_long(BATT_FULL);
+    s = get_status();
 
-        if (percent <= 20)
-            color = "#ff0000";
-        else
-            color = "#00ff00";
+    percent = (lnum1/(lnum2/100));
 
+    if (percent <= 20)
+        color = "#ff0000";
+    else
+        color = "#00ff00";
 
-        if (strcmp(status,"Charging") == 0)
-            s = '+';
-        if (strcmp(status,"Discharging") == 0)
-            s = '-';
-        if (strcmp(status,"Full") == 0)
-            s = '=';
-        free(status);
-        return smprintf("<span color=\"%s\">%c%ld%%</span>", color, s, percent);
-    }
-    else return smprintf("");
+    return smprintf("<span color=\"%s\">%c%ld%%</span>", color, s, percent);
 }
 
 void
 setstatus(char *str)
 {
-	XStoreName(dpy, DefaultRootWindow(dpy), str);
-	XSync(dpy, False);
+    XStoreName(dpy, DefaultRootWindow(dpy), str);
+    XSync(dpy, False);
 }
 
 int
 main(void)
 {
-	char *status;
-	char *timeinfo;
+    char *status;
+    char *timeinfo;
     char *battery;
 
-	if (!(dpy = XOpenDisplay(NULL))) {
-		fprintf(stderr, "dwmstatus: cannot open display.\n");
-		return 1;
-	}
+    if (!(dpy = XOpenDisplay(NULL))) {
+        fprintf(stderr, "dwmstatus: cannot open display.\n");
+        return 1;
+    }
 
-	for (;;sleep(30)) {
-		timeinfo = mktimes("%a %d %b %H:%M %Z %Y", NULL);
+    for (;;sleep(30)) {
+        timeinfo = mktimes("%a %d %b %H:%M %Z %Y", NULL);
         battery = getbattery();
 
-		status = smprintf("%s / %s",
-				 battery, timeinfo);
-		setstatus(status);
+        status = smprintf("%s / %s",
+                battery, timeinfo);
+        setstatus(status);
         free(battery);
         free(timeinfo);
-		free(status);
-	}
+        free(status);
+    }
 
-	XCloseDisplay(dpy);
+    XCloseDisplay(dpy);
 
-	return 0;
+    return 0;
 }
-
